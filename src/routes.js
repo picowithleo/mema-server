87465a3516b02017687e7fa49e76b57dc93c1754
Jinyuan Chen <picowithleo@gmail.com>
2020-10-07 01:10:10 +1000

    add main routes

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+
+
+const express = require("express");
+const postsRoute = require("./routes/posts");
+const router = express.Router();
+
+router.use("/posts", postsRoute);
+
+module.exports = router;
